14ca900a4c06fac0d26471554e770574692bee1d
syncdecodes <syncdecodes@gmail.com>
2026-04-10 10:14:19 +0530

    Add components

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+/* Function App 1 */
+
+
+function Button() {
+    // Inline
+    const styles = {
+        backgroundColor: "black",
+        color: "white"
+    }
+    return (
+        <>
+            <button className="btn" style={styles} >Button Component</button>
+        </>
+    )
+}
+
+export default Button
+
+
---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+export default function Card() {
+    return (
+        <>
+            <div className="card">
+                <img src="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_960_720.png" alt="Github" className="card-image" />
+                <h3>Github <a href="https://github.com/imdevvx">imdevvx</a></h3>
+                <h6>Instagram <a href="https://instagram.com/imdevx">imdevx</a></h6>
+                <p>Hey there! I am Dev<br /> vibing? nope. <br /> Do check my profile mates.</p>
+            </div>
+        </>
+    )
+}
