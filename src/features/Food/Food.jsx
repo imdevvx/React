e60040317e015e5cd88e649bb2945ee678857390
syncdecodes <syncdecodes@gmail.com>
2026-04-11 09:59:05 +0530

    Add src/features Food

---
+++ src/features/Food/Food.jsx
@@ -0,0 +1,15 @@
+export default function Food() {
+    const food1 = "Orange"
+    const food2 = "Banana"
+  return (
+    <>
+    <h2>Food</h2>
+    <p>File path - src/features/Food/Food.jsx</p>
+    <ul>
+        <li>Apple</li>
+        <li>{food1}</li>
+        <li>{food2}</li>
+    </ul>
+    </>
+  )
+}
